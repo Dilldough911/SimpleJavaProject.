9a85b3f5834e4a384f2c1996a242bf5adbdc5119
setia <setiawanbimo2006@gmail.com>
2025-10-20 08:21:03 +0700

    Program Sederhana

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
-void main() {
-    System.out.println("Hello there, this is my Java program for repository!");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello there, this is my updated Java program for repository!");
+
+        String name = "Bagas";
+        int version = 2;
+
+        System.out.println("Program created by: " + name);
+        System.out.println("Repository version: " + version);
+
+        if (version > 1) {
+            System.out.println("This is an improved version of the program!");
+        }
+    }
 }
